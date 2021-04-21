b1c07961c9be4fa4fcc459bb0a1aec5262d9bea6
SyedAbbasHaider <syedhaider066@gmail.com>
2021-04-21 01:02:07 -0400

    First Commit

---
+++ seeds/seedHelper.js
@@ -0,0 +1,25 @@
+module.exports.descriptors = [
+    'Forest',
+    'Ancient',
+    'More',
+    'Fire',
+    'Moral',
+    'Tiger',
+    'King',
+    'Pipe',
+    'Monkey',
+    'Pine'
+]
+
+module.exports.places = [
+    'Flats',
+    'Village',
+    'Town',
+    'Storage',
+    'Station',
+    'Farm',
+    'Empire',
+    'Burger',
+    'Tommy',
+    'Choi'
+]
